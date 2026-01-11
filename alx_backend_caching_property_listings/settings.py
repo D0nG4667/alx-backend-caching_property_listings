625597c11219e31081e4a3c6086f4d86fbb616f4
D0nG4667 <D0nG4667@users.noreply.github.com>
2026-01-11 10:33:02 +0300

    feat: Cache Property List View

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nvg_1u&nwv$g_#3nc5=onzm=kkh_=7*muorc2g1*e^(g#y-!!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["testserver", "127.0.0.1", "localhost"]
 
 
 # Application definition
